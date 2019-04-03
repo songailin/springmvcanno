627abaa1dd7e59480263fa29c66553a903501ac7
songailin <songailin@qq.com>
2019-04-03 11:03:20 +0800

    增加interceptor

---
+++ src/main/java/com/easysoft/interceptor/MyHandlerInterceptor.java
@@ -0,0 +1,22 @@
+package com.easysoft.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyHandlerInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("===MyHandlerInterceptor-->preHandle...===");
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("===MyHandlerInterceptor-->postHandle...===");
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("===MyHandlerInterceptor-->afterCompletion...===");
+    }
+}
